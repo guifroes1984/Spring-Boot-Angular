17064c1608ac9b5b1bc8302e1e390983ca86d9e6
guifroes1984 <guilhermefroes26@gmail.com>
2023-01-03 18:24:41 -0300

    Criando a API de Serviço Prestado, e salvando um serviço prestado pelo Postman.

--- src/main/java/io/github/guifroes/clientes/rest/ServicoPrestadoContoller.java
+++ src/main/java/io/github/guifroes/clientes/rest/ServicoPrestadoContoller.java
@@ -13,6 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -46,5 +47,14 @@ public class ServicoPrestadoContoller {
         servicoPrestado.setValor(bigDecimalConverter.converter(dto.getPreco()));
 
         return repository.save(servicoPrestado);
+    }
+
+    @GetMapping
+    public List<ServicoPrestado> pesquisar(
+            @RequestParam(value = "nome", required = false, defaultValue = "") String nome,
+            @RequestParam(value = "mes", required = false) Integer mes
+
+    ) {
+        return repository.findByNomeClienteAndMes("%" + nome + "%", mes);
     }
 }
