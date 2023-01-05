7d02201d1d4397993b5d916d0870aa9f92dde571
guifroes1984 <guilhermefroes26@gmail.com>
2023-01-05 10:22:02 -0300

    Autenticando com os usuários da base de dados.

--- src/main/java/io/github/guifroes/clientes/model/repository/UsuarioRepository.java
+++ src/main/java/io/github/guifroes/clientes/model/repository/UsuarioRepository.java
@@ -3,5 +3,8 @@ package io.github.guifroes.clientes.model.repository;
 import io.github.guifroes.clientes.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByUsername(String username);
 }
