437bd2444acc5bd984182a913ea85e0eefe8fe24
guifroes1984 <guilhermefroes26@gmail.com>
2023-01-05 12:44:32 -0300

    Melhorando as validações do usuário no lado do servidor.

--- src/main/java/io/github/guifroes/clientes/model/repository/UsuarioRepository.java
+++ src/main/java/io/github/guifroes/clientes/model/repository/UsuarioRepository.java
@@ -7,4 +7,7 @@ import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     Optional<Usuario> findByUsername(String username);
+
+    /*Vai fazer uma contagem de usuários => select count(*) > 0 from usuario where username = :login*/
+    boolean existsByUsername(String username);
 }
